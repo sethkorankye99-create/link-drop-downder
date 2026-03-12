64f6eb0597940a908c25a909284b05074230d522
sethkorankye99-create <sethkorankye99@gmail.com>
2026-03-12 00:33:50 +0000

    Fix duplicate PORT declaration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ytDlp = require('yt-dlp-exec');
 const app = express();
-const PORT = 3000;
+
 
 app.get('/api/download', async (req, res) => {
     const videoUrl = req.query.url;
